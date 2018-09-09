b0c4ac5a1dfbef4a429e77a6da8c2ef9c8061270
christiansiegel <christiansiegel@users.noreply.github.com>
2018-09-09 12:23:08 +0200

    dynamic search button styling

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 $(function() {
   var nav = $("nav");
-  var toggler = $("nav > button.navbar-toggler");
+  var toggler = $("nav button.navbar-toggler");
+  var searchBtn = $("nav button#search-button");
   var header = $("#header");
 
   var navHeight = nav.outerHeight();
@@ -8,20 +9,26 @@ $(function() {
   var update = function(inverseExpanded) {
     return function() {
       var scroll = $(window).scrollTop();
-      var offset = header.outerHeight() - navHeight
-      var expanded = (toggler.attr("aria-expanded") === "true") ^ inverseExpanded
+      var offset = header.outerHeight() - navHeight;
+      var expanded = (toggler.attr("aria-expanded") === "true") ^ inverseExpanded;
       if (scroll >= offset || expanded) {
         nav
           .removeClass('navbar-light')
           .removeClass('bg-transparent')
           .addClass('navbar-dark')
           .addClass('bg-primary');
+        searchBtn
+          .removeClass('btn-outline-dark')
+          .addClass('btn-outline-light');
       } else {
         nav
           .removeClass('navbar-dark')
           .removeClass('bg-primary')
           .addClass('navbar-light')
           .addClass('bg-transparent');
+        searchBtn
+          .removeClass('btn-outline-light')
+          .addClass('btn-outline-dark');
       }
     }
   }
