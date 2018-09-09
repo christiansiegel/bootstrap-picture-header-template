dd3dbfb3b5fd917d2323f9e7018d6c9a8e2f8040
christiansiegel <christiansiegel@users.noreply.github.com>
2018-09-09 12:14:27 +0200

    use jQuery for navbar scroll styling

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-function setNavOffset () {
-  var offset = $("#header").height() - $("nav").outerHeight()
-  $("nav").attr("data-sps-offset", offset);
-}
+$(function() {
+  var nav = $("nav");
+  var toggler = $("nav > button.navbar-toggler");
+  var header = $("#header");
 
-$(document).ready(function() {
-  setNavOffset()
-})
+  var navHeight = nav.outerHeight();
+
+  var update = function(inverseExpanded) {
+    return function() {
+      var scroll = $(window).scrollTop();
+      var offset = header.outerHeight() - navHeight
+      var expanded = (toggler.attr("aria-expanded") === "true") ^ inverseExpanded
+      if (scroll >= offset || expanded) {
+        nav
+          .removeClass('navbar-light')
+          .removeClass('bg-transparent')
+          .addClass('navbar-dark')
+          .addClass('bg-primary');
+      } else {
+        nav
+          .removeClass('navbar-dark')
+          .removeClass('bg-primary')
+          .addClass('navbar-light')
+          .addClass('bg-transparent');
+      }
+    }
+  }
+
+  $(window).scroll(update(false));
+  $(window).resize(update(false));
+  toggler.click(update(true));
+});
