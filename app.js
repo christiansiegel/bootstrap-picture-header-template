afe6b5537527e7401510c957c03ff83830d9f9fb
christiansiegel <christiansiegel@users.noreply.github.com>
2018-09-09 12:41:25 +0200

    light headline

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 $(function() {
   var nav = $("nav");
   var toggler = $("nav button.navbar-toggler");
-  var searchBtn = $("nav button#search-button");
   var header = $("#header");
 
   var navHeight = nav.outerHeight();
@@ -13,22 +12,12 @@ $(function() {
       var expanded = (toggler.attr("aria-expanded") === "true") ^ inverseExpanded;
       if (scroll >= offset || expanded) {
         nav
-          .removeClass('navbar-light')
           .removeClass('bg-transparent')
-          .addClass('navbar-dark')
           .addClass('bg-primary');
-        searchBtn
-          .removeClass('btn-outline-dark')
-          .addClass('btn-outline-light');
       } else {
         nav
-          .removeClass('navbar-dark')
           .removeClass('bg-primary')
-          .addClass('navbar-light')
           .addClass('bg-transparent');
-        searchBtn
-          .removeClass('btn-outline-light')
-          .addClass('btn-outline-dark');
       }
     }
   }
